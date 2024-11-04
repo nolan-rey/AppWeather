646892bbf49e3590da607cb3ea6af15d8417f06f
nolan reymond <nolan.reymond@saintmichelannecy.fr>
2024-11-04 13:20:52 +0100

    design + debut code

--- AppWeather/MainWindow.xaml.cs
+++ AppWeather/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace AppWeather
 {
@@ -23,6 +25,36 @@ namespace AppWeather
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+    
+
+    public async Task<string> GetBlogdetails()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=annecy,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Main main = JsonConvert.DeserializeObject<Main>(content);
+                var temp = main.temp;
+                return temp.ToString();
+            }
+            else
+            {
+                var tt = "Error";
+            }
+            return null;
         }
     }
 }
